4a0de6ec46c62744ee1ce17c6602cf243f139968
Nathan Knowler <nathan@knowler.me>
2019-03-27 21:22:52 -0600

    ⛳

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 document.getElementById('root').innerHTML = 'Basic PWA Example';
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js').then(function() {
-    console.info('Service worker registered.');
-  });
+  navigator.serviceWorker.register('/service-worker.js');
 }
--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,11 @@
-var CACHE_NAME = 'basic-pwa-cache-v1';
-var urlsToCache = [
-  '/',
-  '/app.js',
-  '/app.css',
-];
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache)
-      })
+    caches.open('basic-pwa-cache-v1')
+      .then(cache => cache.addAll([
+        '/',
+        '/app.js',
+        '/app.css',
+      ]))
   );
 });
 
